e272dfea2e008d195659465f6eafc82de2bf0445
Jorge Reis <63481188+jhapreis@users.noreply.github.com>
2020-12-11 12:12:08 -0300

    Update grafico e filtragem
    Melhoria no gráfico do histograma de delta_t e na filtragem das saturações.

--- mainFile.py
+++ mainFile.py
@@ -27,7 +27,7 @@ def peak_finder(series, height): # peak_finder se aplica em data frames com valo
 
 
 ## 1.2.
-def peaks_em_x(df, height):
+def peaks_em_x(df, height, err_mess=0):
     '''
     Esse loop cria e checa se existem erros na obtenção dos picos através da função peak-finder, quando peaks != 2
     '''
@@ -37,13 +37,14 @@ def peaks_em_x(df, height):
     
     for i in range(df.shape[1]):
         evento = df[ df.columns[i] ]
-        _ = peak_finder(evento, height = height)
+        _ = peak_finder(series=evento, height=height)
         if len(_) != 2:
             print(f'problema no {evento.name} com tamanho = {len(_)} e picos em {_}')
             counter += 1
         peaks_em_x.append(_)
 
-    print(f'\nloop finalizado com {counter} erros')
+    if err_mess == 1:
+        print(f'\nloop finalizado com {counter} erros')
 
     return(peaks_em_x)
 
@@ -53,24 +54,26 @@ def peak_finder_xy(series, height):
     '''
     Essa função utiliza a peak_finder e já retorna os dados como uma Series com os valores em x e y de cada pico
     '''
-    __ = peak_finder(series, height = height)
+    __ = peak_finder(series=series, height=height)
     
     return(series.iloc[__])
 
 
 ## 1.4.
-def peaks_em_xy(df, height):
+def peaks_em_xy(df, height, err_mess=0):
+    
     peaks_em_xy = pd.Series([])
 
     for i in range( df.shape[1] ):
         evento = df[ df.columns[i] ]
-        _ = peak_finder_xy(evento, height = height)
+        _ = peak_finder_xy(series=evento, height=height)
         peaks_em_xy = pd.concat([  peaks_em_xy, _  ])
 
     if len(peaks_em_xy) != 2*(df.shape[1] ): # se ele reconhe algo diferente de dois picos por waveform:
         print(f'existe um erro nos picos, pois aparece(m) {len(peaks_em_xy) - 2*(df.shape[1] )} pico(s) a mais')
     else:
-        print('não foram detectados problemas na quantidade de picos')
+        if err_mess == 1:
+            print('não foram detectados problemas na quantidade de picos')
 
     peaks_em_xy = pd.DataFrame(peaks_em_xy).reset_index()
     peaks_em_xy.columns = ['x', 'y']
@@ -79,8 +82,8 @@ def peaks_em_xy(df, height):
 
 
 ##1.5. 
-def peaks_divididos_01(df, height):
-    _ = peaks_em_xy(df = df, height = height)
+def peaks_divididos_01(df, height, err_mess=0):
+    _ = peaks_em_xy(df=df, height=height, err_mess=err_mess)
     peaks_0,   peaks_1   =   _.query('index % 2 == 0'),   _.query('index % 2 != 0')
     return(peaks_0, peaks_1)
 
@@ -116,15 +119,11 @@ def derivada_x(s): # insira uma series na entrada; o valor retornado está em co
 
 ##3.1.
 def contorno_limite_derivada(series, VA_1, VA_2, height): # recebe uma Series do pandas para começar
-    
-    
     '''
     Define um recorte de onde se deve buscar um pulso e os seus delimitadores
     VA_1 e VA_2 são variáveis arbitrárias para definir a largura do pulso 
     '''
-    
-
-    peak = peak_finder(series, height = height) # encontrar os picos da waveform
+    peak = peak_finder(series, height=height) # encontrar os picos da waveform
     
     s1 = series[ (peak[0] - VA_1):(peak[0] + VA_2) ] # recortar envolta dos picos
     s2 = series[ (peak[1] - VA_1):(peak[1] + VA_2) ]
@@ -134,12 +133,10 @@ def contorno_limite_derivada(series, VA_1, VA_2, height): # recebe uma Series do
     df2 = pd.DataFrame( dict(s1 = s2) ).reset_index()
     df2.columns = ['time', 's2']
     
-    
     '''
     Calcular a "derivada" em cada ponto no entorno, para saber os limitantes do pulso
     Ao terminar, retornar o data frame que contem os valores limitantes do contorno do pulso
     '''  
-
     indexLim_1 = derivada_x( df1['s1'] ) # índices limitantes
     indexLim_2 = derivada_x( df2['s2'] )
     
@@ -148,7 +145,6 @@ def contorno_limite_derivada(series, VA_1, VA_2, height): # recebe uma Series do
     df2 = df2.iloc[    [  indexLim_2[0], indexLim_2[-1]  ]    ] 
     
     # print(df2) # series marcada pelas colunas 
-    
     
     # da Series original, temos agora o contorno do pulso
     s1 = series[ df1['time'].iloc[0] : df1['time'].iloc[1]+1 ] # soma 1 para incluir o último termo
@@ -161,17 +157,15 @@ def contorno_limite_derivada(series, VA_1, VA_2, height): # recebe uma Series do
     return(pulsos) # retorna os dois contornos, um de cada pulso
 
 
-##3.2. 
-def contorno_limite_arbitrario(df, VA_1, VA_2, height, check):
+# ##3.2. 
+def contorno_limite_arbitrario(df, VA_1, VA_2, height , err_mess=0):
     '''
         Esse loop serve para olhar, em cada waveform do Data Frame original, o contorno ao redor de cada pulso, selecionado através do pico e das larguras arbitradas.
         Por razões de implementaçao, filtramos como queremos em cada uma das waveforms e transformamos num array. Com isso,
     acho que ficará bem mais rápido de fazer essas operações. O resultado é empilhado e transformado num Data Frame.
     '''
 
-    _ = peaks_divididos_01(df = df, height = height)
-    peaks_0   ,   peaks_1      =     _[0]   ,    _[1]
-
+    peaks_0   ,   peaks_1      =     peaks_divididos_01(df=df, height=height)
     # if len(peaks_0) != len(peaks_1):
     #     print('erro na obtenção dos picos divididos')
 
@@ -215,78 +209,7 @@ def contorno_limite_arbitrario(df, VA_1, VA_2, height, check):
     contorno_1 = pd.DataFrame( np.array(contorno_1) ).T
     
 
-
-    # contorno_0 = np.array([])
-    # contorno_1 = np.array([])
-
-    # for i in range( df.shape[1]  ): 
-        
-    #     evento = df[ df.columns[i] ]
-    #     s0 = evento.where( 
-    #        (evento.index >= peaks_0.x.iloc[i] - VA_1) & (evento.index <= peaks_0.x.iloc[i] + VA_2) 
-    #                     ).dropna().array
-    #     s1 = evento.where( 
-    #        (evento.index >= peaks_1.x.iloc[i] - VA_1) & (evento.index <= peaks_1.x.iloc[i] + VA_2) 
-    #                     ).dropna().array
-
-    #     if len(s1) != 91:
-    #         print(f'erro em {i}')
-
-    #     contorno_0 = np.append( contorno_0,s0 )
-    #     contorno_1 = np.append( contorno_1,s1 )
-
-    # contorno_0 = pd.DataFrame( 
-    #     contorno_0.reshape( (df.shape[1] , 1 + VA_1 + VA_2) ) 
-    #                         )
-
-    # contorno_1 = pd.DataFrame( 
-    #     contorno_1.reshape( (df.shape[1] , 1 + VA_1 + VA_2) ) 
-    #                         )
-
-
-    # contorno_0 = np.zeros(1 + VA_1 + VA_2)
-    # contorno_1 = np.zeros(1 + VA_1 + VA_2)
-
-    # for i in range( df.shape[1]  ): 
-        
-    #     evento = df[ df.columns[i] ]
-    #     s0 = evento.where( 
-    #        (evento.index >= peaks_0.x.iloc[i] - VA_1) & (evento.index <= peaks_0.x.iloc[i] + VA_2) 
-    #                     ).dropna().array
-    #     s1 = evento.where( 
-    #        (evento.index >= peaks_1.x.iloc[i] - VA_1) & (evento.index <= peaks_1.x.iloc[i] + VA_2) 
-    #                     ).dropna().array
-    #     print('pimba')
-    #     contorno_0 = np.vstack( (contorno_0, s0) )
-    #     contorno_1 = np.vstack( (contorno_1, s1) )
-
-
-    """
-    Obs.: O código implementado está como está por uma questão de desempenho, ou seja, de tempo de execução.
-        As justificativas se baseiam não só no tempo, na verdade, mas no fato de que eu não consegui mais guardar
-    as Series dentro dos arrays, como originalmente. Acho que deve ter algo a ver com alguma atualização do Numpy, 
-    mas o resultado agora parece "extender" os dois arrays sem separá-los.
-    Ele é bem mais rápido do que o código "intuitivamente" implementado (usando o pd.concat), como segue:
-    
-
-    contorno_0 = pd.Series([])
-    contorno_1 = pd.Series([])
-     
-    for i in range( df.shape[1]  ): 
-        
-       evento = df[ df.columns[i] ]
-       s0 = evento.where( 
-           (evento.index >= peaks_0.x.iloc[i] - VA_1) & (evento.index <= peaks_0.x.iloc[i] + VA_2) 
-                        ).dropna()
-       s1 = evento.where( 
-           (evento.index >= peaks_1.x.iloc[i] - VA_1) & (evento.index <= peaks_1.x.iloc[i] + VA_2) 
-                        ).dropna()
-        
-       contorno_0 = pd.concat( [contorno_0 , s0] , axis=1 )
-       contorno_1 = pd.concat( [contorno_1 , s1] , axis=1 )
-    """
-
-    if check == 1:
+    if err_mess == 1:
         
         '''
         Loop de checagem dos contornos
@@ -305,15 +228,115 @@ def contorno_limite_arbitrario(df, VA_1, VA_2, height, check):
     if len(aux) != 0:
         print(f'\nForam detectados {len(aux)} problemas na questão do tamanho da janela do contorno;\nOs problemas estão em {aux};\nPreenchidos com valores Nan\n')
     else:
-        print('\nNão foram detectados problemas na questão do tamanho da janela do contorno\n')
+        if err_mess == 1:
+            print('\nNão foram detectados problemas na questão do tamanho da janela do contorno\n')
 
     #Retorna dois DataFrames contendo os contornos de cada waveform para cada um dos dois picos
     return(  contorno_0, contorno_1  ) 
 
 
 
+def contorno_limite_arbitrario_picos(df, peaks_01, VA_1, VA_2, height, err_mess=0):
+    '''
+        Esse loop serve para olhar, em cada waveform do Data Frame original, o contorno ao redor de cada pulso, selecionado através do pico e das larguras arbitradas.
+    Por razões de implementaçao, filtramos como queremos em cada uma das waveforms e transformamos num array. Com isso,
+    acho que ficará bem mais rápido de fazer essas operações. O resultado é empilhado e transformado num Data Frame.
+    '''
 
-###################################################################################################################
+    peaks_xy_0   ,   peaks_xy_1      =     peaks_01
+    
+    '''
+        Para eventos que "terminam na borda" da janela, é possível que tenhamos um problema de tamanho do contorno, 
+    que seria de não pegarmos ou todo o pulso ou de não conseguir alocar tudo num DataFrame por causa da diferença
+    de tamanho.  
+    '''
+    # aux = []
+
+    # for i in range( df.shape[1]  ): 
+        
+    #     evento = df[ df.columns[i] ]
+
+    #     s0 = evento.where( 
+    #        (evento.index >= peaks_0.x.iloc[i] - VA_1) & (evento.index <= peaks_0.x.iloc[i] + VA_2) 
+    #                     ).dropna().array
+    #     if len(s0) != 1 + VA_1 + VA_2:
+    #         s0 = np.append(   s0   ,   np.full(1 + VA_1 + VA_2 - len(s0) , np.nan)    )
+    #         aux.append(i)
+    #     # if len(s0) != 1 + VA_1 + VA_2:
+    #     #         print(f'erro em {i}')
+
+    #     s1 = evento.where( 
+    #        (evento.index >= peaks_1.x.iloc[i] - VA_1) & (evento.index <= peaks_1.x.iloc[i] + VA_2) 
+    #                     ).dropna().array
+    #     if len(s1) != 1 + VA_1 + VA_2:
+    #         s1 = np.append(   s1   ,   np.full(1 + VA_1 + VA_2 - len(s1) , np.nan)    )
+    #         aux.append(i)
+    #     # if len(s1) != 1 + VA_1 + VA_2:
+    #     #         print(f'erro em {i}')
+
+    contorno_0 = []
+    aux_0 = []
+    for i in peaks_xy_0.index:
+        evento = df[ df.columns[i] ]
+        s0 = evento.where( 
+            (evento.index >= peaks_xy_0['x'][i] - VA_1) & (evento.index <= peaks_xy_0['x'][i] + VA_2) 
+                            ).dropna().array
+        if len(s0) != 1 + VA_1 + VA_2:
+            s0 = np.append(   s0   ,   np.full(1 + VA_1 + VA_2 - len(s0) , np.nan)    )
+            aux_0.append(i)
+            # if len(s0) != 1 + VA_1 + VA_2:
+            #         print(f'erro em {i}')
+        contorno_0.append(s0)
+    
+    contorno_1 = []
+    aux_1 = []
+    for i in peaks_xy_1.index:
+        evento = df[ df.columns[i] ]
+        s1 = evento.where( 
+            (evento.index >= peaks_xy_1['x'][i] - VA_1) & (evento.index <= peaks_xy_1['x'][i] + VA_2) 
+                            ).dropna().array
+        if len(s1) != 1 + VA_1 + VA_2:
+            s1 = np.append(   s1   ,   np.full(1 + VA_1 + VA_2 - len(s1) , np.nan)    )
+            aux_1.append(i)
+            # if len(s1) != 1 + VA_1 + VA_2:
+            #         print(f'erro em {i}')
+        contorno_1.append(s1)
+
+    contorno_0 = pd.DataFrame( np.array(contorno_0) ).T # pontos do contorno vs waveform
+    contorno_1 = pd.DataFrame( np.array(contorno_1) ).T
+    
+
+    if err_mess == 1:
+        
+        '''
+        Loop de checagem dos contornos
+        '''
+
+        if ( len(contorno_0) and len(contorno_1) ) != df.shape[1] :
+            _  = abs( len(contorno_0) - len(contorno_1) )
+            __ = 'primeiro' if len(contorno_0) > len(contorno_1) else 'segundo'
+            print(f'Os contornos não batem no tamanho. Existe(m) {_} a mais no {__}')
+
+        for elemento in (contorno_0 or contorno_1):
+            if len(elemento) != VA_1 + VA_2 + 1:
+                print(f'Erro no {elemento.name}')
+    
+    aux = aux_0 + aux_1
+    if len(aux) != 0:
+        print(f'\nForam detectados {len(aux)} problemas na questão do tamanho da janela do contorno;\nOs problemas estão em {aux};\nPreenchidos com valores Nan\n')
+    else:
+        if err_mess == 1:
+            print('\nNão foram detectados problemas na questão do tamanho da janela do contorno\n')
+
+    '''
+    Retorna dois DataFrames contendo os contornos de cada waveform para cada um dos dois picos
+    '''
+    return(  contorno_0, contorno_1  ) 
+
+
+
+
+#%%
 #4. Integrais
 
 ##4.1.
@@ -335,7 +358,8 @@ def integral_simples_dataframe(df, dx):
     return(pd.Series(integrais))
 
 
-###################################################################################################################
+
+#%%
 #5. Base Line
 
 ##5.1.
@@ -349,18 +373,14 @@ def baseLine_sample(series, height, VA_1): # recebe uma series do pandas
     '''Pega todos os elementos até o início do primeiro pulso; intervalo exclusivo à direita'''
     
     if len(_) != 0: # pode acontecer de que o peak_finder não 
-        
         x_inicio_pulso_0 = _[0] - VA_1 # definido arbitrariamente    
         sample  = series.iloc[:x_inicio_pulso_0]
-        
     else:
         sample = None
 
     return( sample ) # este elemento é uma Series
 
-
 ##5.2. Base Line
-
 def baseLine(df, height, VA_1):
     
     baseLines = [] # será uma lista de series
@@ -374,6 +394,122 @@ def baseLine(df, height, VA_1):
 
 
 
-###################################################################################################################
+
+#%%
+#6. LIDANDO COM FILTRAGENS E SATURAÇÕES NO DATA FRAME
+
+def filtra_saturacao_total(df, height, VA_1, VA_2):
+
+    _ = peaks_divididos_01( df, height = height )
+    peaks_xy_0   ,   peaks_xy_1   =   _[0]   ,   _[1]
+
+    filt_0 = pd.Series( peaks_xy_0.query('y > y.min()').index // 2 )
+    filt_1 = pd.Series( peaks_xy_1.query('y > y.min()').index // 2 )
+
+    _ = pd.concat(  (filt_0, filt_1), ignore_index=True  ).value_counts()
+    filt_saturacao = pd.Series (
+    _.where( _ == 2).dropna().sort_index().index
+                            ) # aparece duas vezes <--> está nas duas Series
+
+    df_filt_saturacao = df.iloc[ : , filt_saturacao]
+
+    return (df_filt_saturacao)
+
+def filtra_saturacao_parcial(df, height, VA_1, VA_2):
+
+    _ = peaks_divididos_01( df, height = height )
+    peaks_xy_0   ,   peaks_xy_1   =   _[0]   ,   _[1]
+    filt_0 = peaks_xy_0.query('y > y.min()')
+    filt_1 = peaks_xy_1.query('y > y.min()')
+
+    return( np.array([ filt_0, filt_1 ]) )
+
+def filtra_delta_t(df, convert_to_microsec, time_in_ADCch, height):
+    
+    _ = pd.DataFrame(   peaks_em_x( df=df, height=height),  columns = ['peak_0', 'peak_1']   )   
+    delta_x = (   _['peak_1']  -  _['peak_0']   )
+    delta_x.name = 'delta_x'
+    
+    delta_x_filtrado = delta_x.where(delta_x <= convert_to_microsec*time_in_ADCch).dropna()
+    df_filtrado = df.iloc[: , delta_x_filtrado.index]
+    
+    return(df_filtrado)
+
+
+
+#%% Plot fit exponencial das diferenças de tempo
+
+def curve_fit_exponencial(df, height, convert_to_microsec, fit_point_align=0):
+
+    _ = pd.DataFrame(   peaks_em_x( df=df, height=height),  columns = ['peak_0', 'peak_1']   )   
+    delta_x = (   _['peak_1']  -  _['peak_0']   )
+    delta_x.name = 'delta_x'
+
+    '''
+    (a) Definimos a função de ajuste para a exponencial
+    '''
+    def fit_function(t, A, tau, C): # função de ajuste exponencial
+        return(  A * np.e**(-t/tau) + C  )
+
+    '''
+    (b) Coletamos os dados e fazemos plots preliminares, para ir acompanhando os resultados
+        Obs.: a variável number_of_bins é o jeito padrão de quantificar quantos bins se tem num histograma
+        fonte: https://stackoverflow.com/questions/60512271/how-can-i-extract-the-bins-from-seaborns-kde-distplot-object
+    '''
+    number_of_bins     = min(  len( np.histogram_bin_edges(delta_x, bins="fd") ), 50  )
+    data_entries, bins = np.histogram(delta_x , bins = number_of_bins)
+    bins_centers       = np.array([0.5 * (bins[i] + bins[i+1]) for i in range(len(bins)-1)])
+
+    '''
+    (c) Fazemos a regressão pela curve_fit do Scipy
+        obs.: É importante dar um valor inicial para os parâmetros, como um chute inicial. 
+        Nesse caso, eu apenas chutei os valores mesmo algumas vezes
+    '''
+    p0 = np.array([ 0.5, 10, 1 ]) # initial guess for the parameters: y(t) = A * np.e**(-t/tau) + C
+    opt_coeff, cov_coeff = curve_fit(  fit_function , xdata = bins_centers , ydata = data_entries, p0 = p0  )
+
+    '''
+    (d) Plot com histograma e regressão da curva
+    '''
+        # converter os resultados para as unidades corretas
+    coeff = np.array([ opt_coeff[0], opt_coeff[1]*convert_to_microsec, opt_coeff[2] ])
+        # plot do histograma
+    sns.histplot(delta_x*convert_to_microsec, color='gray', bins=number_of_bins)
+        # plot dos centros dos bins
+    if fit_point_align == 1:
+        plt.scatter(bins_centers*convert_to_microsec, data_entries, color = 'black', label = 'centro dos bins')
+        # plot da curve_fit
+    x = np.linspace(0,10, 10000)
+    plt.plot( 
+        x, fit_function(x, coeff[0], coeff[1], coeff[2]), 
+        color = 'orange', label = f'y(t) = {round(coeff[0])}*e^-t/{round(coeff[1])} + {round(coeff[2])}' )
+        # detalhes
+    plt.title('Diferenças de tempo entre o primeiro e o segundo pulso')
+    plt.ylabel(r'$\dfrac{\Delta N}{\Delta t}$')
+    plt.xlabel(r'Diferença de tempo ($\mu$s)')
+    plt.legend()
+    plt.show()
+    plt.savefig('images/curve_fit-vida_media', dpi = 150)
+
+    '''
+    (e) incertezas
+    '''
+        #Calcula a incerteza
+    coeff_std   = np.sqrt(np.diag(cov_coeff)) #"sigma"
+    coeff_error = coeff_std / np.sqrt(number_of_bins - 1) #"sigma / sqrt(n)"
+        #Converte a incerteza do tau para micro seg
+    coeff_error[1] = coeff_error[1]*convert_to_microsec
+        #Resultado
+    #coeff_error
+    coeff_results = pd.DataFrame( [coeff, coeff_error] ).T
+    coeff_results.rename( columns = {0:'valor', 1:'incerteza'}, index ={ 0:'A', 1:'tau', 2:'C' } , inplace = True )
+    #print(coeff_results)
+
+    return(coeff_results)
+
+
+
+
+#%%
 
 print('mainFile e bibliotecas importados')
