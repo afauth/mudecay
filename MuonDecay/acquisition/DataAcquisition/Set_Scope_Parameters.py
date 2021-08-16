9739ada64e83f6893c670907422d2fa88b382c28
Jorge Reis <63481188+jhapreis@users.noreply.github.com>
2021-08-16 11:21:50 -0300

    Update Set_Scope_Parameters.py

--- MuonDecay/acquisition/DataAcquisition/Set_Scope_Parameters.py
+++ MuonDecay/acquisition/DataAcquisition/Set_Scope_Parameters.py
@@ -104,8 +104,8 @@ def check_parameters(oscilloscope):
         else:
             try_info = False
 
-    trigger_value_mV = 1_000*float( re.split(';', trigger_info)[-1] )
-    trigger_slope = re.split(';', trigger_info)[5]
+    trigger_value_mV = 1_000*float( re.split('TRIGGER:MAIN:LEVEL |\n', trigger_info)[-2] ) # re.split(';', trigger_info)[-1]
+    trigger_slope = re.split('SLOPE |;', trigger_info)[6] #re.split(';', trigger_info)[5]
     y_scale_value = float( re.split('\n', y_scale_info)[0] )
 
     if trigger_value_mV != cfg_scope.trigger:
